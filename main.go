3b3d5c1f14ebd4708c8d6a02173680cfecdec4b8
paradojin <dylhan.aros@sansano.usm.cl>
2023-04-17 19:26:38 -0400

    mejoras

--- main.go
+++ main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Bienvenido a la aplicación de la aerolínea.")
+	// Agrega aquí el código para mostrar el menú y interactuar con la API REST.
+}
